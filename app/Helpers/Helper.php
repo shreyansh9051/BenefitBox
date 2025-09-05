93b9912f22342b2f10b9e6049f2d8e9e9b867e36
Panchal Shreyansh <shreyansh.panchal14@gmail.com>
2025-09-05 17:10:59 +0530

    Fix: Helper's numberFormat function issue

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -9,48 +9,54 @@ class Helper
         return $currencySymbol . number_format($price, 2, '.', ',');
     }
 
-    public static function numberFormat( $n, $precision = 1 )
+    public static function numberFormat($n, $precision = 1)
     {
         if ($n < 900) {
-     
+        
             $n_format = number_format($n, $precision);
-     
+        
             $suffix = '';
-     
+        
         } else if ($n < 900000) {
-     
-            $n_format = number_format($n / 1000, $precision);
-     
+        
+            $value = floor($n / 1000 * pow(10, $precision)) / pow(10, $precision);
+        
+            $n_format = number_format($value, $precision);
+        
             $suffix = 'K';
-     
+        
         } else if ($n < 900000000) {
-     
-            $n_format = number_format($n / 1000000, $precision);
-     
+        
+            $value = floor($n / 1000000 * pow(10, $precision)) / pow(10, $precision);
+        
+            $n_format = number_format($value, $precision);
+        
             $suffix = 'M';
-     
+        
         } else if ($n < 900000000000) {
-     
-            $n_format = number_format($n / 1000000000, $precision);
-     
+        
+            $value = floor($n / 1000000000 * pow(10, $precision)) / pow(10, $precision);
+        
+            $n_format = number_format($value, $precision);
+        
             $suffix = 'B';
-     
+        
         } else {
-     
-            $n_format = number_format($n / 1000000000000, $precision);
-     
+        
+            $value = floor($n / 1000000000000 * pow(10, $precision)) / pow(10, $precision);
+        
+            $n_format = number_format($value, $precision);
+        
             $suffix = 'T';
-     
         }
-    
+
+        if ($precision > 0) {
         
-        if ( $precision > 0 ) {
+            $dotzero = '.' . str_repeat('0', $precision);
         
-            $dotzero = '.' . str_repeat( '0', $precision );
-        
-            $n_format = str_replace( $dotzero, '', $n_format );
+            $n_format = str_replace($dotzero, '', $n_format);
         }
-    
+
         return $n_format . $suffix;
     }
 }
